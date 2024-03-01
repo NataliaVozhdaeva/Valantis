0969545a7955cffe354d803d2a30cba135af47c6
NataliaVozhdaeva <LomzNat@yandex.ru>
2024-03-01 23:43:47 +0400

    feat: add content and fix some styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -30,6 +31,14 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'bundle.css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets',
+          to: 'assets',
+        },
+      ],
     }),
   ],
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
